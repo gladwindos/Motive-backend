f3414b5576415d4fb1c252abba9f097cafd9cbf4
gladwindos <gladwin.dos@hotmail.co.uk>
2016-07-04 15:00:16 +0100

    timezone change

--- src/uni_events/settings.py
+++ src/uni_events/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rloi*h_7!4!p&bx-#d-n^r-)o)58p$q_^okg@m*9zt%hzmhtq1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Belfast'
 
 USE_I18N = True
 
